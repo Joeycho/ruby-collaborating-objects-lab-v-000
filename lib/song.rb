ec8c3482b38057b4e94aa64c8085a640e360116c
Joey Cho <cjy12182000@naver.com>
2018-06-06 12:43:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ class Song
     #  binding.pry
       artist = Artist.find_or_create_by_name(sfilename[0])
       song = self.new(sfilename[1])
-      song.artist = artist
+      artist.add_song(song)
       song
     end
 
