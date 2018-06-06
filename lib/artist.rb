a8a0ad76ee4d9dc7c20ea73183e19bd6d0451f2a
Joey Cho <cjy12182000@naver.com>
2018-06-06 11:08:30 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,6 +10,10 @@ class Artist
     @@all
   end
 
+  def songs
+    Song.all.select{|song| song.artist == self.name}
+  end
+
   def self.find_or_create_by_name(name)
     if self.all.select {|artist| artist.name == name}
     else
