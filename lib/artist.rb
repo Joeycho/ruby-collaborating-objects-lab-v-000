9981d9afe2c94502e229402b603f18ee933650e0
Joey Cho <cjy12182000@naver.com>
2018-06-06 10:59:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,17 @@
 class Artist
   attr_accessor :name
+  @@all =[]
 
   def initialize(name)
     @name = name
   end
 
+  def self.all?
+    @@all? 
+  end
+
   def self.find_or_create_by_name(name)
+    if
     self.new(name)
   end
 end
